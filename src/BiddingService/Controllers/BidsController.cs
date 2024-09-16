ab9e63003411aee027ee801cfb7440ab960a88b7
donkennie <ajeigbekehinde160@gmail.com>
2024-09-16 17:44:09 +0100

    add bid controller and mapper

---
+++ src/BiddingService/Controllers/BidsController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using BiddingService.DTOs;
+using BiddingService.Features.Commands;
+using BiddingService.Features.Queries;
+using KernelShared;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiddingService.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class BidsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IMediator _mediator;
+
+
+        public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint, IMediator mediator)
+        {
+            _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
+            _mediator = mediator;
+        }
+
+
+        [Authorize, HttpPost("{auctionId}/bids")]
+        public async Task<ActionResult<BidDTO>> PlaceBid(string auctionId, int amount)
+        {
+            var command = new BidCommand
+            {
+                AuctionId = auctionId,
+                Amount = amount,
+                Bidder = User.Identity.Name
+            };
+
+            var result = await _mediator.Send(command);
+
+            if (result == null)
+            {
+                return BadRequest("Cannot place bid on this auction.");
+            }
+
+            await _publishEndpoint.Publish(_mapper.Map<BidPlaced>(result));
+
+            return Ok(result);
+        }
+
+        [HttpGet("{auctionId}/bids")]
+        public async Task<ActionResult<List<APIResponse>>> GetBidsForAuction(string auctionId)
+        {
+            var result = await _mediator.Send(new GetBidsForAuctionQuery { AuctionId = auctionId });
+
+            if (result == null || !result.Any())
+            {
+                return NotFound("No bids found for this auction.");
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/BiddingService/Mappers/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BiddingService.DTOs;
+using BiddingService.Entities;
+using KernelShared;
+
+namespace BiddingService.Mappers
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Bid,BidDTO>();
+            CreateMap<Bid, BidPlaced>();
+        }
+    }
+}
