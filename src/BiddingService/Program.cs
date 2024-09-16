66b198696d5e755c0c67d6bd41fe763c81a445f6
donkennie <ajeigbekehinde160@gmail.com>
2024-09-16 19:11:20 +0100

    add bidding configuration

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -1,6 +1,45 @@
+using BiddingService.Data;
+using BiddingService.Features.Commands;
+using BiddingService.Features.Customers;
+using MassTransit;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer()
+       .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(BidCommand).GetTypeInfo().Assembly))
+
+       .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies())
+       .AddControllers();
+
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.UseRetry(r =>
+        {
+            r.Handle<RabbitMqConnectionException>();
+            r.Interval(5, TimeSpan.FromSeconds(10));
+        });
+        cfg.Host(configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +60,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await Policy.Handle<TimeoutException>().WaitAndRetryAsync(5, t => TimeSpan.FromSeconds(10))
+    .ExecuteAndCaptureAsync(async () =>
+    {
+        await DB.InitAsync("BidDb",
+            MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
+        await DbInitializer.InitDb(app);
+    });
 
 app.Run();
