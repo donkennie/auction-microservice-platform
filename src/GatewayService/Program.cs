9eeed72096dbbc3ba5276be81f5d6f0fc5b7fc37
donkennie <ajeigbekehinde160@gmail.com>
2024-09-18 17:37:12 +0100

    config gateway service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,25 +1,57 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var path = Path.Combine(Directory.GetCurrentDirectory(), "readme.md");
+    var desc = $"Auction Service API";
+
+    if (File.Exists(path))
+        desc = File.ReadAllText(path);
+
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = $"Auction Service API",
+        Version = "v1",
+        Description = desc
+    });
+    c.ResolveConflictingActions(x => x.First());
+});
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(settings =>
+    {
+        settings.SwaggerEndpoint("/swagger/v1/swagger.json", "Auction API v1");
+    });
 }
+app.UseCors("customPolicy");
+app.MapReverseProxy();
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
